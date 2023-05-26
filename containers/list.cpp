6a9f2cb14efca87095bba9597ee58f944eaa91f6
Omar-Abdo <o.abdelalim@btu.edu.eg>
2023-05-26 17:46:15 +0300

    C++ Trains and Labs_new

--- containers/list.cpp
+++ containers/list.cpp
@@ -5,8 +5,8 @@ int main()
 {
     std::forward_list<int> firstList(10), secondList;
 
-    firstList.assign({1,2,3,4});
-    secondList.assign(10,20);
+    firstList.assign({1,2,3,4,5,6,7,8,9,10});
+    secondList.assign(11,20);
     secondList.assign(firstList.begin(), firstList.end());
 
     for (auto element : secondList)
