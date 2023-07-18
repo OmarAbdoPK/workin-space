6382827a35519891a1cf57f58eaf182125bbcc33
OmarAbdoPK <o.abdelalim@btu.edu.eg>
2023-07-18 20:38:34 +0300

    Data Structure

--- Data_Structure/arrayBasedList.cpp
+++ Data_Structure/arrayBasedList.cpp
@@ -7,8 +7,8 @@ class arrayListType
 {
 public:
 	arrayListType(int size = 100);
-	arrayListType(arrayListType& otherList); //copy constructor	
-	~arrayListType(); // destructor
+	arrayListType(arrayListType &otherList); //copy constructor
+	~arrayListType();						 // destructor
 
 	bool isEmpty();
 	bool isFull();
@@ -19,22 +19,23 @@ public:
 	void insertAt(int loc, int item);
 	void insertEnd(int item);
 	void removeAt(int loc);
-	void retrieveAt(int loc, int& item);
+	void retrieveAt(int loc, int &item);
 	void replaceAt(int loc, int item);
 	void clearList();
 	int seqSearch(int item);
 	void insertNoDuplicate(int item);
 	void remove(int item);
+
 private:
-	int *list; 	//array to hold the list elements
-    int length;		//to store the length of the list
-    int maxSize;		//to store the maximum size of the list
+	int *list;	 //array to hold the list elements
+	int length;	 //to store the length of the list
+	int maxSize; //to store the maximum size of the list
 };
 
 arrayListType::arrayListType(int size)
 {
-	/* initailize the private members */
-	if(size <= 0)
+	/* initialize the private members */
+	if (size <= 0)
 	{
 		cout << " Wrong Size " << endl;
 		maxSize = 100;
@@ -43,24 +44,24 @@ arrayListType::arrayListType(int size)
 		maxSize = size;
 
 	length = 0;
-	list = new int [maxSize];
+	list = new int[maxSize];
 	assert(list != NULL); //terminate if unable to allocate memory space
 }
 
-arrayListType::arrayListType(arrayListType& otherList)
+arrayListType::arrayListType(arrayListType &otherList)
 {
 	maxSize = otherList.maxSize;
 	length = otherList.length;
-	list = new int [maxSize]; 	//create the array
-	assert(list != NULL);	//terminate if unable to allocate memory space
+	list = new int[maxSize]; //create the array
+	assert(list != NULL);	 //terminate if unable to allocate memory space
 
-	for(int j = 0; j < length; j++)  //copy otherList
-		list [j] = otherList.list[j];
+	for (int j = 0; j < length; j++) //copy otherList
+		list[j] = otherList.list[j];
 }
 
 arrayListType::~arrayListType()
 {
-	delete [] list;
+	delete[] list;
 }
 
 bool arrayListType::isEmpty()
@@ -85,14 +86,14 @@ int arrayListType::maxListSize()
 
 void arrayListType::print()
 {
-	for(int i = 0; i < length; i++)
-		cout<<list[i]<<" ";
-	cout<<endl;
+	for (int i = 0; i < length; i++)
+		cout << list[i] << " ";
+	cout << endl;
 }
 
 bool arrayListType::isItemAtEqual(int loc, int item)
 {
-	if(loc < 0 || loc >= length)
+	if (loc < 0 || loc >= length)
 		return false;
 	else
 		return (list[loc] == item);
@@ -100,30 +101,30 @@ bool arrayListType::isItemAtEqual(int loc, int item)
 
 void arrayListType::insertAt(int loc, int item)
 {
-	if(isFull())
-		cout<<" The List is Full " << endl;
-	else if(loc < 0 || loc > length)
+	if (isFull())
+		cout << " The List is Full " << endl;
+	else if (loc < 0 || loc > length)
 		cout << "Out of Range " << endl;
 	else
 	{
-		for(int i = length; i > loc; i--)
-			list[i] = list[i - 1];	//shift right
-		
-		list[loc] = item;	//insert the item at the specified position
-		length++;	//increment the length
+		for (int i = length; i > loc; i--)
+			list[i] = list[i - 1]; //shift right
+
+		list[loc] = item; //insert the item at the specified position
+		length++;		  //increment the length
 	}
 }
 
 void arrayListType::insertEnd(int item)
 {
-	if(isFull())
-		cout<<" The List is Full " << endl;
+	if (isFull())
+		cout << " The List is Full " << endl;
 	else
 		list[length++] = item;
 }
-void arrayListType::retrieveAt(int loc, int& item)
+void arrayListType::retrieveAt(int loc, int &item)
 {
-	if(loc < 0 || loc >= length)
+	if (loc < 0 || loc >= length)
 		cout << "Out of Range " << endl;
 	else
 		item = list[loc];
@@ -131,7 +132,7 @@ void arrayListType::retrieveAt(int loc, int& item)
 
 void arrayListType::replaceAt(int loc, int item)
 {
-	if(loc < 0 || loc >= length)
+	if (loc < 0 || loc >= length)
 		cout << "Out of Range " << endl;
 	else
 		list[loc] = item;
@@ -144,43 +145,43 @@ void arrayListType::clearList()
 
 int arrayListType::seqSearch(int item)
 {
-	for(int loc = 0; loc < length; loc++)
-		if(list[loc] == item)
+	for (int loc = 0; loc < length; loc++)
+		if (list[loc] == item)
 			return loc;
 	return -1;
 }
 
 void arrayListType::insertNoDuplicate(int item)
 {
-	if(isFull())
-		cout<<" The List is Full " << endl;
+	if (isFull())
+		cout << " The List is Full " << endl;
 	else
 	{
 		int flag = seqSearch(item);
-		if(flag == -1)
+		if (flag == -1)
 			list[length++] = item;
 		else
-			cout<<"No duplicates are allowed."<<endl;
-	}			
+			cout << "No duplicates are allowed." << endl;
+	}
 }
 
 void arrayListType::remove(int item)
 {
 	int loc = seqSearch(item);
-	if(loc == -1)
-		cout<<"The item to be deleted is not in the list" << endl;
+	if (loc == -1)
+		cout << "The item to be deleted is not in the list" << endl;
 	else
 		removeAt(loc);
 }
 
 void arrayListType::removeAt(int loc)
 {
-	if(loc < 0 || loc >= length)
-    	cout<<"The location of the item to be removed is out of range."<<endl;
+	if (loc < 0 || loc >= length)
+		cout << "The location of the item to be removed is out of range." << endl;
 	else
 	{
-   		for(int i = loc; i < length - 1; i++)
-	 		list[i] = list[i+1];
+		for (int i = loc; i < length - 1; i++)
+			list[i] = list[i + 1];
 
 		length--;
 	}
@@ -189,19 +190,19 @@ void arrayListType::removeAt(int loc)
 int main()
 {
 	arrayListType lst1;
-	for(int i = 0; i < 20; i++)
+	for (int i = 0; i < 20; i++)
 		lst1.insertAt(i, i * i);
 
-    lst1.print();
-    
-    int x;
-	lst1.retrieveAt(10, x);
-	
-    cout<< x << endl;
+	lst1.print();
 
-    arrayListType lst2(lst1);
-    
-    lst2.print();
+	int x;
+	lst1.retrieveAt(10, x);
+
+	cout << x << endl;
+
+	arrayListType lst2(lst1);
+
+	lst2.print();
 
 	return 0;
 }
